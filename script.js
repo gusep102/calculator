cc7b1006930bd69925d47671fa3eb6dcc18b2d98
Aki <43755568+gperalta102@users.noreply.github.com>
2023-08-28 22:24:44 -0700

    Allow for screen to be updated when there is a button pressed

--- script.js
+++ script.js
@@ -29,16 +29,35 @@ function operate(num1, num2, ope) {
     }
 }
 
+function updateDisplay(string) {
+    screenDisplay = document.querySelector('.display');
+    console.log(screenDisplay.textContent);
+    screenDisplay.textContent = string;
+
+}
+
 // test
 
 let a = 4;
-let b =5;
+let b = 5;
 
 // variables
-let num1;
-let num2;
+let num1 = '';
+let num2 = '';
 let ope;
 
+// button listeners
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+
+        
+
+
+
+    })
+})
 
 
 // test
