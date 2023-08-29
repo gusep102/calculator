ad747e08c283917879c0f8fcfe1fe8c2fa54309f
Aki <43755568+gperalta102@users.noreply.github.com>
2023-08-28 20:37:55 -0700

    Create the calculator functions add, sub, multi, and divide

---
+++ script.js
@@ -0,0 +1,25 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// test
+
+let a = 4;
+let b =5;
+
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
